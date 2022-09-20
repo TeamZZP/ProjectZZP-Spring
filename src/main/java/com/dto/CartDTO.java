41f432873e577a69370e25d1f78d0b3943cedbf3
dmlwjd1921 <wjd1921@gmail.com>
2022-09-20 17:03:28 +0900

    create cartConteroller,dto,dao,service,mapper,jsp

---
+++ src/main/java/com/dto/CartDTO.java
@@ -0,0 +1,24 @@
+package com.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CartDTO {
+	int cart_id;
+	int p_id;
+	String userid;
+	int p_amount;
+	String p_image;
+	String p_name;
+	int p_selling_price;
+	String cart_created;
+	int money;
+}

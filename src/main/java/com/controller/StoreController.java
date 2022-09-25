a854563ae4cc503a9b89c934231f95391223841b
munbi <munbi0328@gmail.com>
2022-09-25 23:50:03 +0900

    add zzim function

--- src/main/java/com/controller/StoreController.java
+++ src/main/java/com/controller/StoreController.java
@@ -8,8 +8,11 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.dto.CategoryDTO;
@@ -23,21 +26,16 @@ public class StoreController {
 	@Autowired
 	StoreService service;
 	
+	//스토어메인
 	@RequestMapping(value = "/store")
 	public ModelAndView storeMain(HttpSession session) {
 		ModelAndView mav = new ModelAndView();
 		HashMap<String, String> map = new HashMap<String, String>();
 		List<Integer> zzimList = new ArrayList<Integer>();
-		int zzim ;
 		MemberDTO mdto = (MemberDTO) session.getAttribute("login");  //로그인세션
 		List<ProductByCategoryDTO> bestProdudctlist = service.bestProduct();  //베스트상품리스트
 		if(mdto !=null) {//로그인이 되었을 경우 찜 가져오기
-			for (int i = 0; i < bestProdudctlist.size(); i++) {   //개별 상품번호로 찜 가져오기
-				System.out.println(mdto.getUserid()+" , "+bestProdudctlist.get(i).getP_id());
-				map.put("userid", mdto.getUserid());
-				map.put("p_id", String.valueOf(bestProdudctlist.get(i).getP_id()));	
-				zzimList.add(service.zzimCheck(map));
-			}
+			zzimList=service.zzimAllCheck(mdto.getUserid());
 		}
 		System.out.println("zzimList: "+zzimList);
 		List<CategoryDTO> categoryList = service.category(); //카테고리 리스트
@@ -52,8 +50,8 @@ public class StoreController {
 	}
 	
 	//카테고리 변경시
-	@RequestMapping(value = "/productByCategory")
-	public ModelAndView productByCategory(HttpSession session ,@RequestParam("c_id") int c_id) {
+	@RequestMapping(value = "/store/{c_id}")
+	public ModelAndView productByCategory(HttpSession session ,@PathVariable("c_id") int c_id) {
 		HashMap<String, String> map = new HashMap<String, String>();
 		List<Integer> zzimList = new ArrayList<Integer>();
 		ModelAndView mav = new ModelAndView();
@@ -61,12 +59,7 @@ public class StoreController {
 		List<ProductByCategoryDTO> prodByCateList = service.productByCategory(c_id);  //해당카테고리상품리스트
 		System.out.println(prodByCateList);
 		if(mdto !=null) {//로그인이 되었을 경우 찜 가져오기
-			for (int i = 0; i < prodByCateList.size(); i++) {   //개별 상품번호로 찜 가져오기
-				System.out.println(mdto.getUserid()+" , "+prodByCateList.get(i).getP_id());
-				map.put("userid", mdto.getUserid());
-				map.put("p_id", String.valueOf(prodByCateList.get(i).getP_id()));	
-				zzimList.add(service.zzimCheck(map));
-			}
+			zzimList=service.zzimAllCheck(mdto.getUserid());
 		}
 		mav.addObject("Productlist",prodByCateList);  
 		List<CategoryDTO> categoryList = service.category(); //카테고리 List 
@@ -76,29 +69,51 @@ public class StoreController {
 		return mav;
 	}
 	
-	@RequestMapping(value = "/productRetrieve")
-	public ModelAndView productRetrieve(HttpSession session ,@RequestParam("p_id") int p_id) {
+	//제품상세
+	@RequestMapping(value = "/product/{p_id}")
+	public ModelAndView productRetrieve(HttpSession session ,@PathVariable("p_id") int p_id) {
 		ModelAndView mav = new ModelAndView();
 		HashMap<String, String> map = new HashMap<String, String>();
-		int zzim =0;
+		List<Integer> zzimList = new ArrayList<Integer>();
 		MemberDTO mdto = (MemberDTO) session.getAttribute("login");  //로그인세션
 		ProductByCategoryDTO pdto= service.productRetrieve(p_id);  //선택상품상세
 		if(mdto!=null) {
 			map.put("userid", mdto.getUserid());
 			map.put("p_id", String.valueOf(pdto.getP_id()));
-			zzim = service.zzimCheck(map);
+			zzimList=service.zzimAllCheck(mdto.getUserid());
 		}
 		List<CategoryDTO> categoryList = service.category(); //카테고리 List 
 		mav.addObject("categoryList", categoryList);
 		mav.addObject("pdto",pdto);
 		mav.addObject("mdto", mdto);
-		mav.addObject("zzim", zzim);
+		mav.addObject("zzim", zzimList);
 		mav.setViewName("productRetrieve");
 		
 		return mav;
 	}
 	
-	
+	//찜기능
+	@RequestMapping("/zzim")
+	public @ResponseBody int zzim(@RequestParam("p_id") int p_id , HttpSession session) {
+		MemberDTO mdto = (MemberDTO) session.getAttribute("login");
+		HashMap<String,String> map = new HashMap<String,String>();
+		List<Integer> zzimList = new ArrayList<Integer>();
+		map.put("userid", mdto.getUserid());
+		map.put("p_id", String.valueOf(p_id));
+		int zzimCheck = service.zzimCheck(map); //찜 검사(찜했는지안했는지)
+		int zzimData = 0;
+		if(zzimCheck==0) {    //찜 안했을때 
+			service.addZzim(map);  //찜 추가
+			zzimList=(service.zzimAllCheck(mdto.getUserid()));
+			zzimData = 1;
+		}else {   //찜 했을때
+			service.deleteZzim(map);  //찜 제거
+			zzimList=(service.zzimAllCheck(mdto.getUserid()));
+			zzimData=0;
+		}
+		
+		return zzimData;
+	}
 	
 
 }

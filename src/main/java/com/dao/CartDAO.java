16b7f8d187243d52e60df44f45e4e06a3cee4a52
dmlwjd1921 <wjd1921@gmail.com>
2022-09-27 12:28:57 +0900

    update cartList, delete cartList

--- src/main/java/com/dao/CartDAO.java
+++ src/main/java/com/dao/CartDAO.java
@@ -1,5 +1,7 @@
 package com.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -22,6 +24,44 @@ public class CartDAO {
 	public int cartCount(String userid) {
 		int n = session.selectOne("CartMapper.cartCount",userid);
 		return n;
+	}
+
+	public int sum_money(String userid) {
+		int sum = session.selectOne("CartMapper.sum_money",userid);
+		return sum;
+	}
+
+	public int checkCart(HashMap<String, Object> map) {
+		int n = session.selectOne("CartMapper.checkCart",map);
+		return n;
+	}
+
+	public void cartAdd(CartDTO cart) {
+		int n = session.insert("CartMapper.cartAdd",cart);
+		System.out.println("장바구니insert 갯수>>"+n);
+	}
+
+	public void updateCart(CartDTO cart) {
+		int n = session.update("CartMapper.updateCart",cart);
+		System.out.println("장바구니 수량 변경 >>"+n);
+		
+	}
+
+	public void cartDel(String cart_id) {
+		int n = session.delete("CartMapper.cartDel", cart_id);
+		System.out.println("상품 삭제>>"+n);
+		
+	}
+
+	public void cartUpdate(HashMap<String, Object> map) {
+		int n = session.update("CartMapper.cartUpdate", map);
+		System.out.println("상품 수량 update>> "+n);
+		
+	}
+
+	public void cartAllDel(ArrayList<String> list) {
+		session.delete("CartMapper.cartAllDel",list);
+		System.out.println("장바구니 비우기성공");
 	}
 	
 	

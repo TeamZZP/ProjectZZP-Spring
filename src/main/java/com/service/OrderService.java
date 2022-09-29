11dd0e16f83f6e85de84d4802224a1a5d63adb03
Cho Yeram <109123745+yeramcho@users.noreply.github.com>
2022-09-29 15:05:56 +0900

    Update order, coupon (#51)
    * update
    
    * fix
    
    * fix
    
    * create couponDTO,OrderController,orders.jsp
    
    Co-authored-by: munbi <munbi0328@gmail.com>

--- src/main/java/com/service/OrderService.java
+++ src/main/java/com/service/OrderService.java
@@ -1,11 +1,13 @@
 package com.service;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dao.OrderDAO;
+import com.dto.MemberCouponDTO;
 import com.dto.PageDTO;
 
 @Service
@@ -17,6 +19,11 @@ public class OrderService {
 	public PageDTO myOrder(Map<String, String> map) {
 		PageDTO dto = dao.myOrder(map);
 		return dto;
+	}
+
+	public List<MemberCouponDTO> selectAllCoupon(String userid) {
+		List<MemberCouponDTO> couponList = dao.selectAllCoupon(userid);
+		return couponList;
 	}
 	
 }

ef21dc66861c94f705e799b11b76a540435eccb7
Cho Yeram <109123745+yeramcho@users.noreply.github.com>
2022-09-30 18:03:57 +0900

    Fix error (#67)
    * Fix error

---
+++ src/main/java/com/dto/ProfileDTO.java
@@ -0,0 +1,18 @@
+package com.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter 
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ProfileDTO {
+	private String userid;
+	private String profile_img;
+	private String profile_txt;
+}

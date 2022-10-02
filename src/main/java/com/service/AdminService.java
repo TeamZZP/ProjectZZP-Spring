a4c563290a9e8e231416dcfedc29619de6881f9f
binsucha <binsucha@gmail.com>
2022-10-03 07:59:01 +0900

    update manage Member

--- src/main/java/com/service/AdminService.java
+++ src/main/java/com/service/AdminService.java
@@ -1,11 +1,14 @@
 package com.service;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dao.AdminDAO;
+import com.dto.AddressDTO;
+import com.dto.MemberDTO;
 import com.dto.PageDTO;
 
 @Service
@@ -22,6 +25,26 @@ public class AdminService {
 	//관리자페이지 회원 관리 : 전체 회원 목록
 	public PageDTO selectAllMember(HashMap<String, String> map) {
 		return dao.selectAllMember(map);
+	}
+	
+	//관리자 페이지 회원 관리 : 회원 삭제
+	public void deleteMember(String userid) {
+		dao.deleteMember(userid);
+	}
+	
+	//관리자 페이지 회원 관리 : 회원 정보 조회
+	public MemberDTO selectMember(String userid) {
+		return dao.selectMember(userid);
+	}
+	
+	//관리자 페이지 회원 관리 : 회원 배송지 목록 조회
+	public List<AddressDTO> selectAllAddress(String userid) {
+		return dao.selectAllAddress(userid);
+	}
+	
+	//관리자 페이지 회원 관리 : 회원 정보 수정
+	public void updateMember(HashMap<String, String> map) {
+		dao.updateMember(map);
 	}
 
 }

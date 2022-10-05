2dfa52d154fa7d46f0caee58cb02479e230c3cc7
Cho Yeram <109123745+yeramcho@users.noreply.github.com>
2022-10-05 17:49:15 +0900

    Add admin order page (#102)
    * Add admin challenge update and delete
    
    * Add admin order page

--- src/main/java/com/service/ChallengeService.java
+++ src/main/java/com/service/ChallengeService.java
@@ -184,6 +184,12 @@ public class ChallengeService {
 		adao.updateChallThisMonth(updateMap);
 	}
 
+
+	public void updateOrder(HashMap<String, String> map) {
+		dao.updateOrder(map);
+	}
+
+
 	
 
 	

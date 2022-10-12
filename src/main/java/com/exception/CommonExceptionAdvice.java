9b6f9b856872fc33b4418f4b753aa857867294f0
Cho Yeram <109123745+yeramcho@users.noreply.github.com>
2022-10-12 17:44:46 +0900

    Edit upload path (#193)
    * Edit and improve
    
    * Edit image edge
    
    * Edit upload path
    
    * Edit upload path

--- src/main/java/com/exception/CommonExceptionAdvice.java
+++ src/main/java/com/exception/CommonExceptionAdvice.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-//@ControllerAdvice
+@ControllerAdvice
 public class CommonExceptionAdvice {
 	 
 	@ExceptionHandler(value = NoHandlerFoundException.class)

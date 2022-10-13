42fa4473d4fd6b219fa369a9e5229740eb2e06fd
choyeram <choyeram8@gmail.com>
2022-10-13 10:59:37 +0900

    Edit interceptor

--- src/main/java/com/interceptor/LoginInterceptor.java
+++ src/main/java/com/interceptor/LoginInterceptor.java
@@ -4,7 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.FlashMapManager;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter {
 
@@ -14,6 +17,11 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 		System.out.println("preHandle ========== "+handler);
 		HttpSession session = request.getSession();
 		if (session.getAttribute("login") == null) {
+			FlashMap flashMap = new FlashMap();
+			flashMap.put("mesg", "로그인이 필요합니다.");
+			FlashMapManager flashMapManager = RequestContextUtils.getFlashMapManager(request);
+			flashMapManager.saveOutputFlashMap(flashMap, request, response);
+			
 			response.sendRedirect("/zzp/login"); 
 			return false;
 		}
